ec9746ed80e876aa588c8fdbbfd30b3e969e9183
Isra Akmal <isra.akmal@rolustech.net>
2024-03-25 01:21:54 +0500

    Your commit message

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import Homework from './Components/Homework';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+     <BrowserRouter>
+   
+   <Routes>
+
+     <Route path="/homework" element={<Homework/>} />
+     <Route path="/homework" element={<Homework/>} />
+     <Route path="/homework" element={<Homework/>} />
+     <Route path="/homework" element={<Homework/>} />
+     <Route path="/homework" element={<Homework/>} />
+     <Route path="/homework" element={<Homework/>} />
+     <Route path="/homework" element={<Homework/>} /> 
+   </Routes>
+ </BrowserRouter>
     <App />
   </React.StrictMode>
 );
